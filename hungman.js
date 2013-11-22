7e8f84989689b314ffdf207a0af3f5b7d0612aec
James Stanley <james@incoherency.co.uk>
2013-11-22 15:55:53 +0000

    Add probabilistic solver

--- hungman.js
+++ hungman.js
@@ -1,5 +1,66 @@
 (function() {
     var refresh = function() {
+        refresh_dict();
+        refresh_markov();
+    };
+
+    var refresh_markov = function() {
+        var gamestate = $('#game-state').val().toLowerCase();
+        var triedletters = $('#tried-letters').val().toLowerCase();
+
+        var tried = {};
+        for (var i = 0; i < gamestate.length; i++)
+            tried[gamestate.charAt(i)] = true;
+        for (var i = 0; i < triedletters.length; i++)
+            tried[triedletters.charAt(i)] = true;
+
+        var score = {};
+
+        var lastchar = '$';
+        for (var i = 0; i < gamestate.length; i++) {
+            var nextchar = '$';
+            if (i < gamestate.length-1)
+                nextchar = gamestate[i+1];
+
+            if (gamestate[i] == '_' && lastchar != '_') {
+                for (var j = 0; j < 26; j++) {
+                    var c = String.fromCharCode('a'.charCodeAt(0) + j);
+                    if (tried[c])
+                        continue;
+
+                    if (!score[c])
+                        score[c] = 0;
+
+                    if (markov[lastchar][c]) {
+                        if (nextchar != '_' && markov[c][nextchar])
+                            score[c] += markov[lastchar][c] * markov[c][nextchar];
+                        else if (nextchar == '_')
+                            score[c] += markov[lastchar][c] * 0.1;
+                    }
+                }
+            }
+
+            lastchar = gamestate[i];
+        }
+
+        var letters = [];
+        for (var c in score)
+            letters.push(c);
+        letters.sort(function(a,b) {
+            return score[b] - score[a];
+        });
+
+        var out = '';
+        for (var i = 0; i < letters.length; i++) {
+            out += '<b>' + letters[i] + '</b>';
+            if (i < letters.length-1)
+                out += ', ';
+        }
+
+        $('#markov-output').html(out);
+    };
+
+    var refresh_dict = function() {
         var gamestate = $('#game-state').val().toLowerCase();
         var triedletters = $('#tried-letters').val().toLowerCase();
 
